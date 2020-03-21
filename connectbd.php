68c1c6993158ca4c45af7c92d88cacfbb37f2ec7
Ksenia Ya <yankovich.k.s@gmail.com>
2020-03-21 11:15:14 +0100

    add get without db

--- connectbd.php
+++ connectbd.php
@@ -10,7 +10,7 @@
         $link = mysqli_connect($host, $user, $password, $db);
 
         if (!$link) {
-            die('Erreur de connexion');
+            return get_file_name_without_bd($title);
         } 
 
         $requete = "SELECT * FROM information_gps WHERE title = \"".$title."\"";
@@ -24,5 +24,17 @@
             }
         }
         return NULL;
+    }
+
+    function get_file_name_without_bd($title){
+        if ($title=="Track 1"){
+            return "1180778.kml";
+        }
+        elseif ($title=="Track 2"){
+            return "1181066.kml";
+        }
+        else{
+            return NULL;
+        }
     }
 ?>
--- translator.php
+++ translator.php
@@ -4,7 +4,7 @@ include("connectbd.php");
 $title = $_POST['title'];
 
 $file = get_file_name_from_bd($title);
-
+/*console_log($file);*/
 $xml_str = file_get_contents($file);
 $xml = simplexml_load_string($xml_str);
 $json = json_encode($xml);
